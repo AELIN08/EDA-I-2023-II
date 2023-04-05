6f8fe80bb7572519781754855ed6289aba7d44ad
AELIN08 MB <evlin.student@gmail.com>
2023-04-05 03:23:54 +0000

    Errores en las funciones Empty, rear

--- P5/SistemaTurnos.c
+++ P5/SistemaTurnos.c
@@ -39,8 +39,14 @@ struct Queue* createQueue(unsigned capacity)
 }
 
 //OPEREACIONES A UTILIZAR PARA PILA
+// La pila esta llena cuando el tope es igual al ultimo indice
+int isStackFull(struct Stack* stack)
+{
+	return stack->top == stack->capacity - 1;
+}
+
 // La pila esta vacia cuando el tope es igual a -1
-int isEmpty(struct Stack* stack)
+int isStackEmpty(struct Stack* stack)
 {
 	return stack->top == -1;
 }
@@ -48,16 +54,15 @@ int isEmpty(struct Stack* stack)
 // Funcion para agregar un elemento a la pila. Incrementa el tope en 1
 void push(struct Stack* stack, int item)
 {
-	if (isFull(stack))
+	if (isStackFull(stack))
 		return;
 	stack->array[++stack->top] = item;
-	printf("%d push a la pila\n", item);
 }
 
 // Funcion para quitar un elemento de la pila. Decrementa el tope en 1
 int pop(struct Stack* stack)
 {
-	if (isEmpty(stack))
+	if (isStackEmpty(stack))
 		return INT_MIN;
 	return stack->array[stack->top--];
 }
@@ -65,7 +70,7 @@ int pop(struct Stack* stack)
 // Funcion para regresar el valor del tope de la pila sin quitarlo
 int peek(struct Stack* stack)
 {
-	if (isEmpty(stack))
+	if (isStackEmpty(stack))
 		return INT_MIN;
 	return stack->array[stack->top];
 }
@@ -73,33 +78,35 @@ int peek(struct Stack* stack)
 //OPERACIONES PARA COLA
 // La cola esta llena cuando se vuelve
 // igual a la capacidad
-int isFull(struct Queue* queue)
+int isQueueFull(struct Queue* queue)
 {
 	return (queue->size == queue->capacity);
 }
 
 // La cola esta vacia cuando su tamano es cero
-int isEmpty(struct Queue* queue)
+int isQueueEmpty(struct Queue* queue)
 {
 	return (queue->size == 0);
 }
 
 // Funcion para agregar un elemento a la cola.
 // Cambia la parte trasera y el tamano.
+
 void enqueue(struct Queue* queue, int item)
 {
-	if (isFull(queue))
+	if (isQueueFull(queue))
 		return;
 	queue->rear = (queue->rear + 1) % queue->capacity;
 	queue->array[queue->rear] = item;
 	queue->size = queue->size + 1;
-	printf("%d agregado a la cola\n", item);
+	
 }
+
 // Funcion para eliminar un elemento de la cola.
 // Cambia el frente y el tamano.
 int dequeue(struct Queue* queue)
 {
-	if (isEmpty(queue))
+	if (isQueueEmpty(queue))
 		return INT_MIN;
 	int item = queue->array[queue->front];
 	queue->front = (queue->front + 1) % queue->capacity;
@@ -110,19 +117,19 @@ int dequeue(struct Queue* queue)
 // Funcion para obtener el frente de la cola.
 int front(struct Queue* queue)
 {
-	if (isEmpty(queue))
+	if (isQueueEmpty(queue))
 		return INT_MIN;
 	return queue->array[queue->front];
 }
 
 // Funcion para obtener la parte trasera de la cola
-
 int rear(struct Queue* queue)
 {
-	if (isEmpty(queue))
+	if (isQueueEmpty(queue))
 		return INT_MIN;
 	return queue->array[queue->rear];
 }
+
 main(){
 	int i, lastItem;
 
@@ -137,7 +144,7 @@ main(){
 
     while (1) {
 
-        printf("\n\t *** BIENVENIDX AL SISTEMA DE TURNOS ***\n");
+        printf("\n\t *** BIENVENIDX AL SISTEMA DE TURNOS ***\n\n");
         printf("1. AGREGAR \n");
         printf("2. SALIR \n");
         printf("¿Que opcion desea?: ");
@@ -146,33 +153,37 @@ main(){
         switch (opcion){
 
             case 1:
-
-                if isEmpty (stack) {
-					printf("ya no hay turnos disponibles");
+				printf("\n************************************************************************\n\n");
+                if (isStackEmpty (stack)) {
+					printf("ya no hay turnos disponibles :c");
 				}
 				else{
 					//Para obtener el ultimo elemento de la pila y agregarlo a cla cola
 					lastItem=pop(stack);
-					enqueue(queue, lastItem)
-
-
+					enqueue(queue, lastItem);
+					printf("El elemento se encuentra formado en la cola con el turno: %d", lastItem);
 				}
+				printf("\n\n************************************************************************\n");
                 break;
             
             case 2:
-
-                printf("Ultimo elemento en tope de la pila: %d\n", peek(stack));
-				printf("El elemento de al final es: %d\n", rear(queue));
+				printf("\n************************************************************************\n\n");
+				if (isStackEmpty (stack)) {
+					printf("Ultimo elemento en tope de la pila: vacio");
+				}
+				else
+                	printf("Ultimo elemento en tope de la pila: %d\n", peek(stack));
+				
+				printf("Ultimo elemento formado en la cola: %d\n", rear(queue));
+				printf("\n\n************************************************************************\n");
+                return 0;
                 break;
 
             default:
                 printf("\nIngresaste algo incorrecto\n");
 
-
         }
     }
-
     return 0;
 }
-
     
