a9bfd3523552737552db905a7fe14a9ea1578f5d
AELIN08 MB <evlin.student@gmail.com>
2023-04-09 22:06:08 +0000

    detalles

--- P5/SistemaTurnos.c
+++ P5/SistemaTurnos.c
@@ -170,6 +170,7 @@ main(){
 				printf("\n************************************************************************\n\n");
 				if (isStackEmpty (stack)) {
 					printf("Ultimo elemento en tope de la pila: vacio");
+					printf("Ultimo elemento formado en la cola: %d\n", rear(queue));
 				}
 				else
                 	printf("Ultimo elemento en tope de la pila: %d\n", peek(stack));
